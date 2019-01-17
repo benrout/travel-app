824402e3bf65ae2f0c9820286e8ccfe72bb15f2a
Ben Rout <benadamrout@gmail.com>
2019-01-17 18:43:36 +0000

    Adds Quotes component

---
+++ src/components/Quotes/Quotes.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { QuotesListProps } from '../../models/QuotesModel';
+
+export function Quotes(props: QuotesListProps) {
+    return (
+        <div>
+            TEST
+            {props.quotes.map(quote => {
+                return (
+                    <div className="quote" key={quote.author}>
+                        <div className="quote__text font-italic">"{quote.text}"</div>
+                        <div className="quote__author">{quote.author}</div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
