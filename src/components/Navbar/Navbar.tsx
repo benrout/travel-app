b7b537fe952c2bdbda2fce6d35e54da6fbac510e
Ben Rout <benadamrout@gmail.com>
2019-01-17 18:44:01 +0000

    Adds navbar logo and right aligns rest of links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,12 +3,15 @@ import * as React from 'react';
 export function Navbar(props) {
     return (
         <div className="d-flex flex-row">
-            <a href="#">Flights</a>
-            <a href="#">Hotels</a>
-            <a href="#">Car Hire</a>
-            <a href="#">Holidays</a>
-            <a href="#">Discover</a>
-            <a href="#">Trip Finder</a>
+            <a href="#">Logo</a>
+            <div className="ml-auto">
+                <a href="#">Flights</a>
+                <a href="#">Hotels</a>
+                <a href="#">Car Hire</a>
+                <a href="#">Holidays</a>
+                <a href="#">Discover</a>
+                <a href="#">Trip Finder</a>
+            </div>
         </div>
     )
 }
