6bc188d866923604bf75002019531ca6e91ba008
Ben Rout <benadamrout@gmail.com>
2019-01-28 18:37:07 +0000

    Fixes trending destinations layout

--- src/components/TrendingDestinations/TrendingDestinations.tsx
+++ src/components/TrendingDestinations/TrendingDestinations.tsx
@@ -9,17 +9,19 @@ export function TrendingDestinations(props: TrendingDestinationListProps) {
             <hr></hr>
             <div className="text-center">Trending {props.destinationType}</div>
             <div className="text-center">The most searched {props.destinationType} on TravelApp</div>
-            <div className="d-flex flex-row flex-wrap">
+            <div className="row">
                 {props.destinations.map(destination => {
                     const imageStyles = {
                         backgroundImage: `url(${destination.imageUrl})`
                     }
                     return (
-                        <div key={destination.title} className="col-lg-4 trending-destination row">
-                            <div className="col-6 trending-destination__image" style={imageStyles}></div>
-                            <div className="col-6 trending-destination__details">
-                                <div>Flights to</div>
-                                <div>{destination.title}</div>
+                        <div key={destination.title} className="col-lg-4">
+                            <div className="trending-destination d-flex flex-row">
+                                <div className="trending-destination__image w-50" style={imageStyles}></div>
+                                <div className="trending-destination__details w-50">
+                                    <div>Flights to</div>
+                                    <div>{destination.title}</div>
+                                </div>
                             </div>
                         </div>
                     )
