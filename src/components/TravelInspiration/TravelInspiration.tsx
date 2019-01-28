686b9a4a24563407aca2a4df0e0e3efaf530404f
Ben Rout <benadamrout@gmail.com>
2019-01-28 18:36:39 +0000

    Removes redundant hr from Travel inspiration component

--- src/components/TravelInspiration/TravelInspiration.tsx
+++ src/components/TravelInspiration/TravelInspiration.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 export function TravelInspiration(props) {
     return (
         <div>
+            <hr></hr>
             <div>{props.heading}</div>
             <div>{props.description}</div>
         </div>
